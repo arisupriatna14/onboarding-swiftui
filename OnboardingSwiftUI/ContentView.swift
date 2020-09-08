b6b9585487a8fa934420b54d8fdc3d56d0c263a3
arisupriatna14 <arisupriatna703@gmail.com>
2020-09-08 18:10:41 +0700

    Initial Commit

---
+++ OnboardingSwiftUI/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  OnboardingSwiftUI
+//
+//  Created by Ari Supriatna on 08/09/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OnboardingSwiftUI/OnboardingSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingSwiftUIApp.swift
+//  OnboardingSwiftUI
+//
+//  Created by Ari Supriatna on 08/09/20.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
